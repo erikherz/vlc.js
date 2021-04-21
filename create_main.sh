733e2654b17031409dc31e66b040c89310490284
Mehdi Sabwat <mehdisabwat@gmail.com>
2021-04-21 10:17:37 +0200

    update patchset and demo

--- create_main.sh
+++ create_main.sh
@@ -27,8 +27,8 @@ PROJECT_DIR=$(pwd)/vlc
 
 cd vlc/build-emscripten
 # for release, remove profiling-funcs and add -Os
-EMCC_DEBUG=1 emcc --bind -O0 -s USE_PTHREADS=1 -s TOTAL_MEMORY=1GB -s PTHREAD_POOL_SIZE=10 \
-     -s OFFSCREEN_FRAMEBUFFER=1 -s LLD_REPORT_UNDEFINED --profiling-funcs \
+emcc --bind -O0 -s USE_PTHREADS=1 -s TOTAL_MEMORY=1GB -s PTHREAD_POOL_SIZE=10 \
+     -s OFFSCREEN_FRAMEBUFFER=1 -s USE_WEBGL2=1 --profiling-funcs \
      -I $PROJECT_DIR/include/ -I $PROJECT_DIR/contrib/wasm32-unknown-emscripten/include/ main.c \
      $PROJECT_DIR/build-emscripten/lib/.libs/libvlc.a \
      vlc-modules.bc $PROJECT_DIR/build-emscripten/modules/.libs/*.a \
