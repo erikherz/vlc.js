40e257836983d6b17b631b37c3c77b2b350b1820
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2021-10-20 11:31:49 +0200

    create_main: Fix paths to binaries
    The VLC buildscript now builds everything in vlc/build-emscripten and
    contrib/wasm32-unknown-emscripten, instead of doing everything in a
    subfolder in vlc/extras/package/wasm-emscripten

--- create_main.sh
+++ create_main.sh
@@ -25,7 +25,6 @@ fi
 
 PATH_VLC=${PATH_VLC:=./vlc}
 SAMPLE_DIR=${SAMPLE_DIR:=./samples}
-PROJECT_DIR=${PROJECT_DIR:=./vlc/extras/package/wasm-emscripten/build}
 
 # For release builds, remove '--profiling-funcs' and add '-Os'
 # Note that we use '-s MODULARIZE', but no '-s EXPORT_ES6', which would
@@ -34,13 +33,13 @@ emcc --bind -s USE_PTHREADS=1 -s TOTAL_MEMORY=1GB -s PTHREAD_POOL_SIZE=15 \
     -s OFFSCREEN_FRAMEBUFFER=1 -s USE_WEBGL2=1 --profiling-funcs \
     -s MODULARIZE=1 -s EXPORT_NAME="VlcModule" \
     -s EXTRA_EXPORTED_RUNTIME_METHODS="allocateUTF8" \
-    -I $PATH_VLC/include/ -I $PROJECT_DIR/wasm32-unknown-emscripten/include/ \
+    -I $PATH_VLC/include/ -I $PATH_VLC/wasm32-unknown-emscripten/include/ \
     main.c exports_media_player.c exports_media.c \
-    $PROJECT_DIR/build-emscripten/lib/.libs/libvlc.a \
-    $PROJECT_DIR/build-emscripten/vlc-modules.bc \
-    $PROJECT_DIR/build-emscripten/modules/.libs/*.a \
-    $PROJECT_DIR/wasm32-unknown-emscripten/lib/*.a \
-    $PROJECT_DIR/build-emscripten/src/.libs/libvlccore.a \
-    $PROJECT_DIR/build-emscripten/compat/.libs/libcompat.a \
+    $PATH_VLC/build-emscripten/lib/.libs/libvlc.a \
+    $PATH_VLC/build-emscripten/vlc-modules.bc \
+    $PATH_VLC/build-emscripten/modules/.libs/*.a \
+    $PATH_VLC/contrib/wasm32-unknown-emscripten/lib/*.a \
+    $PATH_VLC/build-emscripten/src/.libs/libvlccore.a \
+    $PATH_VLC/build-emscripten/compat/.libs/libcompat.a \
      --js-library lib/wasm-imports.js \
     -o experimental.js --preload-file ${SAMPLE_DIR}
