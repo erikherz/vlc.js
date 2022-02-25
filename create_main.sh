1c7a395f1b58d30e12f1ca6615f777378771756c
Alexandre Janniaux <ajanni@videolabs.io>
2022-02-25 15:09:16 +0100

    create_main: build main.c

--- create_main.sh
+++ create_main.sh
@@ -33,28 +33,29 @@ SAMPLE_DIR=${SAMPLE_DIR:=./samples}
 #    -s TRACE_WEBGL_CALLS=1 \
 
 
-# emcc --bind -s USE_PTHREADS=1 -s TOTAL_MEMORY=2GB -s PTHREAD_POOL_SIZE=21 \
-#     -s OFFSCREEN_FRAMEBUFFER=1\
-#     -s USE_WEBGL2=1 \
-#     --profiling-funcs \
-#     -s OFFSCREENCANVAS_SUPPORT=1 \
-#     -s MODULARIZE=1 -s EXPORT_NAME="initModule" \
-#     -s EXTRA_EXPORTED_RUNTIME_METHODS="[allocateUTF8]" \
-#     -s ASYNCIFY=1 -O3 \
-#     -s GL_DEBUG=1 \
-#     -s GL_ASSERTIONS=1 \
-#     -s GL_TRACK_ERRORS=1 \
-#     -I $PATH_VLC/include/ \
-#     main.c exports_media_player.c exports_media.c \
-#     -s EXPORTED_FUNCTIONS=@libvlc_wasm.sym \
-#     $PATH_VLC/build-emscripten/lib/.libs/libvlc.a \
-#     $PATH_VLC/build-emscripten/vlc-modules.bc \
-#     $PATH_VLC/build-emscripten/modules/.libs/*.a \
-#     $PATH_VLC/contrib/wasm32-unknown-emscripten/lib/*.a \
-#     $PATH_VLC/build-emscripten/src/.libs/libvlccore.a \
-#     $PATH_VLC/build-emscripten/compat/.libs/libcompat.a \
-#     --js-library lib/wasm-imports.js \
-#     -o experimental.js --preload-file ${SAMPLE_DIR}
+emcc --bind -s USE_PTHREADS=1 -s TOTAL_MEMORY=2GB -s PTHREAD_POOL_SIZE=21 \
+    -s OFFSCREEN_FRAMEBUFFER=1\
+    -s USE_WEBGL2=1 \
+    --profiling-funcs \
+    -s OFFSCREENCANVAS_SUPPORT=1 \
+    -s MODULARIZE=1 -s EXPORT_NAME="initModule" \
+    -s EXTRA_EXPORTED_RUNTIME_METHODS="[allocateUTF8]" \
+    -s ASYNCIFY=1 -O3 \
+    -s GL_DEBUG=1 \
+    -s GL_ASSERTIONS=1 \
+    -s GL_TRACK_ERRORS=1 \
+    -I $PATH_VLC/include/ \
+    main.c exports_media_player.c exports_media.c \
+    -s EXPORTED_FUNCTIONS=@libvlc_wasm.sym \
+    -s EXPORTED_FUNCTIONS=_main \
+    $PATH_VLC/build-emscripten/lib/.libs/libvlc.a \
+    $PATH_VLC/build-emscripten/vlc-modules.bc \
+    $PATH_VLC/build-emscripten/modules/.libs/*.a \
+    $PATH_VLC/contrib/wasm32-unknown-emscripten/lib/*.a \
+    $PATH_VLC/build-emscripten/src/.libs/libvlccore.a \
+    $PATH_VLC/build-emscripten/compat/.libs/libcompat.a \
+    --js-library lib/wasm-imports.js \
+    -o experimental.js --preload-file ${SAMPLE_DIR}
 
 
 em++ --bind -s USE_PTHREADS=1 -s TOTAL_MEMORY=2GB -s PTHREAD_POOL_SIZE=21 \
