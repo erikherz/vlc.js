353d92a7ad7540ca95ffe77bf180386b9ac577cf
Mehdi Sabwat <mehdi@videolabs.io>
2022-06-15 14:02:49 +0200

    wip: access filesystem demo

--- create_main.sh
+++ create_main.sh
@@ -35,7 +35,7 @@ SAMPLE_DIR=${SAMPLE_DIR:=./samples}
 
 emcc --bind -s USE_PTHREADS=1 -s TOTAL_MEMORY=2GB -s PTHREAD_POOL_SIZE=21 \
     -s OFFSCREEN_FRAMEBUFFER=1\
-    -s USE_WEBGL2=1 -s WASMFS=1 \
+    -s USE_WEBGL2=1 \
     --profiling-funcs \
     -s OFFSCREENCANVAS_SUPPORT=1 \
     -s MODULARIZE=1 -s EXPORT_NAME="initModule" \
