72bd19cac0f7ef4acd0ed6e700bcc44774a31a79
Mehdi Sabwat <mehdi@videolabs.io>
2022-06-27 12:54:36 +0200

    add tested hash

--- compile.sh
+++ compile.sh
@@ -29,7 +29,7 @@ if [ ! -d emsdk ]; then
 fi
 
 cd $WORK_DIR
-TESTED_HASH="271d3552"
+TESTED_HASH="06e361b127e4609e429909756212ed5e30e7d032"
 # Go go go vlc
 if [ ! -d vlc ]; then
     diagnostic "VLC source not found, cloning"
